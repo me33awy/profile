c2e88e951838d491d85fcb8c9ab698c334bbb5a1
me33awy <me33awy@gmail.com>
2023-03-21 15:32:21 +0200

    Add header section

---
+++ src/js/header.js
@@ -0,0 +1,30 @@
+const mobileMenu = document.querySelector("#mobile-menu");
+const navigationBar = document.querySelector(".menu-links");
+
+const closeBtn = document.querySelector(".menu-links span");
+const deleteFunction = false;
+function toggleNav() {
+   document.body.style.backgroundColor = "rgb(124, 124, 124)";
+  navigationBar.classList.toggle("open");
+}
+function closeMenu() {
+  document.body.style.background = "none";
+  navigationBar.classList.remove("open");
+}
+mobileMenu.addEventListener("click", toggleNav);
+closeBtn.addEventListener("click", closeMenu);
+
+const logoText = document.querySelector("#logo h1");
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+function getWindowSize() {
+  if (window.innerWidth <= 768) {
+    logoText.innerHTML = "Menu";
+  } else {
+     logoText.innerHTML = "Mekkawy.";
+     navigationBar.classList.remove("open");
+     document.body.style.background = "none";
+  }
+}
+getWindowSize();
+window.addEventListener("resize", getWindowSize);
