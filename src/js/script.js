310498b26d93d2b2c5016807fc0a099afc645cc4
me33awy <me33awy@gmail.com>
2023-05-02 11:29:13 +0300

    Styling header section

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,21 @@
-import "./header.js";
+const mobileMenu = document.querySelector("#mobile-menu");
+// console.log(mobileMenu)
+const mobileNav = document.querySelector(".menu-links");
+// console.log(mobileNav);
+mobileMenu.addEventListener("click", () => {
+     mobileNav.classList.toggle("open-close");
+     mobileMenu.classList.toggle("style-menu");
+});
+const windowWidth = window.innerWidth;
+const logo = document.querySelector("#logo h1");
+// console.log(windowWidth);
+window.addEventListener("resize", changeName());
 
-// This for change CSS file depending on size of screen
-const linkTag = document.getElementsByTagName("link")[0];
-// console.log(linkTag);
-let windowWidthNumber = 578;
-
-function changeLinkAttribute() {
-  const mobileElements = document.querySelectorAll(".mobile");
-  const desktopElements = document.querySelectorAll(".desktop");
-  // console.log(mobileElements.length);
-
-  for(const ele of desktopElements){
-    if(window.innerWidth <= windowWidthNumber){
-      ele.style.display = "none";
-    }else{
-      ele.style.display = "block";
-    }
-    for(const ele of mobileElements){
-      if(window.innerWidth > windowWidthNumber){
-        ele.style.display = "none";
-      }else{
-        ele.style.display = "block";
-      }
-    }
-  }
-
-  if (window.innerWidth <= windowWidthNumber) {
-    linkTag.setAttribute("href", "./src/css/indexx1h.css");
-    // console.log(linkTag);
-  } else {
-    linkTag.setAttribute("href", "./src/css/indexx0h.css");
-    // console.log(linkTag);
-  }
+function changeName() {
+     if (windowWidth <= 678) {
+          logo.innerText = "Menu";
+     } else {
+          logo.innerText = "Mekkawy.";
+     }
 }
-
-changeLinkAttribute();
-window.addEventListener("resize",changeLinkAttribute);
-
+changeName();
